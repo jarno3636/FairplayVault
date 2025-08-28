714f80dfaf66cb0e1f8d56a4e597dbb3ccf6d733
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-28 15:07:14 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     extend: {
       colors: { brand: { DEFAULT: '#0B0F13', accent: '#22D3EE', soft: '#0F172A' } },
